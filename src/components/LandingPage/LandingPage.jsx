a437c8d5ab38edbbb3dc19b45b5ac3aa35bdf8a0
Stephen Leisy <72354883+stephen-leisy@users.noreply.github.com>
2021-06-10 18:05:42 -0700

    worked on landing page styling and brought in tan-astoria font. (#4)
    Co-authored-by: Stephen Leisy <stephenleisy@Stephens-MacBook-Pro.local>

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './LandingPage.css';
 
 const LandingPage = ({ setLanding }) => {
-  return <h1 onClick={() => setLanding(false)}>Socket Jockey</h1>;
+  return (
+    <main>
+      <h1 onClick={() => setLanding(false)} className={style.colorChange}>
+        Socket Jockey
+      </h1>
+    </main>
+  );
 };
 
 LandingPage.propTypes = {

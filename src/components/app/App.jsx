f729dd601a22ffd2b473612b24f423f03bbe6fdb
Anthony Rosario <anthonymrosario225@gmail.com>
2021-06-14 16:17:47 -0700

    fixed everything about the landing page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,28 +1,54 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CollabWorld from '../CollabRoomPage/CollabWorld';
 import LandingPage from '../LandingPage/LandingPage';
 import RoomsPage from '../RoomsPage/RoomsPage';
-import Footer from './Footer';
+import SoloWorld from '../SoloRoomPage/SoloWorld';
+// import Footer from './Footer';
+// import Header from './Header';
+import './App.css';
 import Header from './Header';
-import styles from './App.css';
+import Footer from './Footer';
 
 const App = () => {
-  const [landing, setLanding] = useState(true);
-  const [room, setRoom] = useState('');
+  // const [landing, setLanding] = useState(true);
+  // const [room, setRoom] = useState('');
 
-  if(landing)
-    return (
-      <main style={{
-        backgroundImage: 'url(landing.png)',
-      }} className={styles.landing}>
-        <LandingPage setLanding={setLanding} />
-      </main>
-    );
   return (
-    <main>
-      <Header room={room} setRoom={setRoom} />
-      <RoomsPage setRoom={setRoom} room={room} />
+    <Router>
+      <Switch>
+        <Route 
+          exact path = "/"
+          component={LandingPage}
+        />
+      </Switch>
+      <Header /> 
+      <Switch>
+        
+          
+        <Route 
+          exact path = "/rooms"
+          component={RoomsPage}
+        />
+
+        <Route 
+          exact path = "/rooms/solo"
+          component={SoloWorld}
+        />
+
+        <Route 
+          exact path = "/rooms/collab"
+          component={CollabWorld}
+        />
+
+        <Route 
+          exact path = "/about"
+          // component={AboutDevs}
+        />
+        
+      </Switch>
       <Footer />
-    </main>
+    </Router>
   );
 };
 

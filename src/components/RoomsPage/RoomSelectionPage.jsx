5435d378bdb5e5b1863db602ac39cdebabd6ae7e
Tyler Farris <TylerpFarris@gmail.com>
2021-06-15 16:28:07 -0600

    Tyler material UI styling (#11)
    * room selection page materail added(grid)
    
    * footer
    
    * grid for room selector done
    
    * finished the room selection material-ui

--- src/components/RoomsPage/RoomSelectionPage.jsx
+++ src/components/RoomsPage/RoomSelectionPage.jsx
@@ -1,36 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './RoomsPage.css';
-
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const RoomSelectionPage = () => {
   return (
-    <article>
-      <Link to="/rooms/solo">
-        <section
-          // onClick={() => setRoom('solo')}
-          className={styles.soloContainer}
-        >
-          <img
-            src="/solo.png"
-            alt="solo world experience"
-            className={styles.soloImage}
-          />
-        </section>
-      </Link>
-      <Link to="/rooms/collab">
-        <section
-          // onClick={() => setRoom('collab')}
-          className={styles.collabContainer}
-        >
-          <img
-            src="/collab.png"
-            alt="collaborative world experience"
-            className={styles.collabImage}
-          />
-        </section>
-      </Link>
-    </article>
+    <Container
+      style={{
+        height: '100vh',
+        overflow: 'hidden',
+      }}
+      maxWidth={false}
+    >
+      <Grid container direction="row" wrap="wrap">
+        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+          <div
+            style={{
+              height: '100vh',
+            }}
+          >
+            <Link to="/rooms/solo">
+              <img
+                src="/solo.png"
+                alt="solo world experience"
+                className={styles.soloImage}
+                style={{
+                  marginTop: '30%',
+                }}
+              />
+            </Link>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={6} xl={6}>
+          <div
+            style={{
+              height: '100vh',
+            }}
+          >
+            <Link to="/rooms/collab">
+              <img
+                src="/collab.png"
+                alt="collaborative world experience"
+                className={styles.collabImage}
+                style={{
+                  justifyContent: 'center',
+                }}
+              />
+            </Link>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 

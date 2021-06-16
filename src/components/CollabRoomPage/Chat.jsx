cb1622b7f8ce56b1c806151f2f45fe66af854a84
Erich Sauer <erich.sauer@gmail.com>
2021-06-16 12:42:19 -0700

    adds chat styling

--- src/components/CollabRoomPage/Chat.jsx
+++ src/components/CollabRoomPage/Chat.jsx
@@ -5,17 +5,16 @@ import styles from './Chat.css';
 
 const Chat = ({ socketRef }) => {
   const [input, setInput] = useState('');
-  const [display, setDisplay] = useState(['', '', '', '', '', '']);
-  const msgCount = useRef([]);
+  const [display, setDisplay] = useState([]);
+  const msgCountRef = useRef([]);
 
   useEffect(() => {
-    socketRef.current.on('server chat', msg => {
+    socketRef.current.on('server chat', (msg) => {
       setDisplay((prev) => {
-        const subArr = prev.slice(1);
-        return [...subArr, msg];
+        // const subArr = prev.length > 5 ? prev.slice(1) : prev;
+        msgCountRef.current = [...prev, msg];
+        return [...prev, msg];
       });
-      const subArr = msgCount.current.slice(1);
-      msgCount.current = [...subArr, msg];
     });
   }, []);
 
@@ -29,11 +28,23 @@ const Chat = ({ socketRef }) => {
     setInput(e.target.value);
   };
   return (
-    <div className={styles.chatDivWrapper}> 
-      <ul className={styles.chatDivFull} >{display && display.map((msg, i) => {
-        return <li key={i} className={styles.chatMessages}>{msg}</li>;
-      }
-      )}
+    <div className={styles.chatDivWrapper}>
+      <ul className={styles.chatUl}>
+        {display &&
+          display.map((msg, i) => {
+            return (
+              <li
+                key={i}
+                className={
+                  i < display.length - 5
+                    ? styles.chatFloat
+                    : styles.chatMessages
+                }
+              >
+                {msg}
+              </li>
+            );
+          })}
       </ul>
 
       <form onSubmit={handleSubmit}>
@@ -41,15 +52,17 @@ const Chat = ({ socketRef }) => {
           margin="none"
           variant="outlined"
           onChange={handleInputChange}
-          value={input} 
-          type="text" 
-          placeholder="chat input">
-        </TextField>
+          value={input}
+          type="text"
+          placeholder="chat input"
+        ></TextField>
         <Button
           style={{ height: '56px' }}
-          size="large" 
-          variant="outlined" 
-          type ="sumbit">send
+          size="large"
+          variant="outlined"
+          type="sumbit"
+        >
+          send
         </Button>
       </form>
     </div>

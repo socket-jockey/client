467fc3942836b5f7e15506374fffabb4fafd670c
Tyler Farris <TylerpFarris@gmail.com>
2021-06-15 23:07:30 -0600

    Tyler (#21)
    * room selection page materail added(grid)
    
    * footer
    
    * grid for room selector done
    
    * finished the room selection material-ui
    
    * styling rome page
    
    * drawer styling
    
    * styling

--- src/components/controls/WorldControls.jsx
+++ src/components/controls/WorldControls.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Controls.css';
 import {
   // Button,
   Container,
@@ -20,49 +21,55 @@ const WorldControls = ({
   reverbAmount,
   handleReverbChange,
 }) => {
-
   return (
     <Container>
-      <FormGroup>
-        <InputLabel>
-          X Gravity
-          <Slider
-            value={gravity.x}
-            onChange={(_, value) => handleGravityChange('x', value)}
-            min={-1}
-            max={1}
-            step={0.1}
-            valueLabelDisplay="auto"
-          />
-        </InputLabel>
-        <InputLabel>
-          Y Gravity
-          <Slider
-            value={gravity.y}
-            onChange={(_, value) => handleGravityChange('y', value)}
-            min={-1}
-            max={1}
-            step={0.1}
-            valueLabelDisplay="auto"
-          />
-        </InputLabel>
-        <InputLabel>
-          Reverb
-          <Slider
-            // value={reverb}
-            value={reverbAmount}
-            // onChange={(_, value) => setReverb(value)}
-            onChange={handleReverbChange}
-            min={0}
-            max={100}
-            valueLabelDisplay="auto"
-          />
-        </InputLabel>
-      </FormGroup>
+      <Container
+        styles={{
+          marginTop: '1rem',
+        }}
+      >
+        <FormGroup>
+          <InputLabel>
+            X Gravity
+            <Slider
+              value={gravity.x}
+              onChange={(_, value) => handleGravityChange('x', value)}
+              min={-1}
+              max={1}
+              step={0.1}
+              valueLabelDisplay="auto"
+            />
+          </InputLabel>
+          <InputLabel>
+            Y Gravity
+            <Slider
+              value={gravity.y}
+              onChange={(_, value) => handleGravityChange('y', value)}
+              min={-1}
+              max={1}
+              step={0.1}
+              valueLabelDisplay="auto"
+            />
+          </InputLabel>
+          <InputLabel>
+            Reverb
+            <Slider
+              // value={reverb}
+              value={reverbAmount}
+              // onChange={(_, value) => setReverb(value)}
+              onChange={handleReverbChange}
+              min={0}
+              max={100}
+              valueLabelDisplay="auto"
+            />
+          </InputLabel>
+        </FormGroup>
+      </Container>
       <ToggleButtonGroup
         value={pause}
         onChange={handlePause}
         exclusive
+        className={styles.toggleGroup}
       >
         <ToggleButton value="paused">pause motion</ToggleButton>
       </ToggleButtonGroup>
@@ -70,6 +77,7 @@ const WorldControls = ({
         value={vibe}
         onChange={handleSettingTheVibe}
         exclusive
+        className={styles.toggleGroup}
       >
         <ToggleButton value="MAJOR">major</ToggleButton>
         <ToggleButton value="MINOR">minor</ToggleButton>
@@ -88,13 +96,13 @@ WorldControls.propTypes = {
   handlePause: PropTypes.func.isRequired,
   gravity: PropTypes.shape({
     x: PropTypes.number.isRequired,
-    y: PropTypes.number.isRequired
+    y: PropTypes.number.isRequired,
   }),
   handleGravityChange: PropTypes.func.isRequired,
   vibe: PropTypes.string.isRequired,
   handleSettingTheVibe: PropTypes.func.isRequired,
   reverbAmount: PropTypes.number.isRequired,
-  handleReverbChange: PropTypes.func.isRequired
+  handleReverbChange: PropTypes.func.isRequired,
 };
 
 export default WorldControls;
